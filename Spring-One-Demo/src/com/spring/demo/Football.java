4ba02f45945ebada69abf1337c7213217b27f4b5
Febin Zachariah <Febin Zachariah>
2017-03-09 13:07:28 -0500

    first commit

---
+++ Spring-One-Demo/src/com/spring/demo/Football.java
@@ -0,0 +1,17 @@
+package com.spring.demo;
+
+public class Football implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Dribble for 10 minute";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Spring-One-Demo/src/com/spring/demo/MyApp.java
@@ -0,0 +1,12 @@
+package com.spring.demo;
+
+public class MyApp {
+	
+	public static void main(String args[])
+	{
+		
+		Coach trackCoach=new TrackCoach();
+		System.out.println(trackCoach.getDailyWorkout());
+	}
+
+}
